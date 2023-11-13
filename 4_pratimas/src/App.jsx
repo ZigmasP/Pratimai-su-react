b3fa507928f8c0fbf8fd90a577b5e8db217c76d3
ZigmasP <147707987+ZigmasP@users.noreply.github.com>
2023-11-13 18:22:30 +0200

    add background image

--- 4_pratimas/src/App.jsx
+++ 4_pratimas/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import BackgroundImage from './components/BackgroundImage';
+
 
 const App = () => {
   return (
     <div className="app-container">
-      <BackgroundImage />
+
     </div>
 
     
